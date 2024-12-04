8bbcd3d70d99a134e664ab514089479053f61bd2
negahibr <ibrahimkhelnegah@gmail.com>
2024-12-04 18:05:03 +0100

    nice

--- getting-started/src/test/java/org/acme/getting/started/GreetingResourceTest.java
+++ getting-started/src/test/java/org/acme/getting/started/GreetingResourceTest.java
@@ -1,35 +1,34 @@
-package org.acme.getting.started;
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-import java.util.UUID;
-
-import org.junit.jupiter.api.Test;
-
-import io.quarkus.test.junit.QuarkusTest;
-
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
+    @Test    
     public void testHelloEndpoint() {
         given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("hello"));
+          .when().get("/hello")
+          .then()
+             .statusCode(200)    
+             .body(is("Hello from Quarkus REST"));
     }
 
     @Test
     public void testGreetingEndpoint() {
         String uuid = UUID.randomUUID().toString();
         given()
-                .pathParam("name", uuid)
-                .when().get("/hello/greeting/{name}")
-                .then()
-                .statusCode(200)
-                .body(is("hello " + uuid));
+          .pathParam("name", uuid)
+          .when().get("/hello/greeting/{name}")
+          .then()
+            .statusCode(200)
+            .body(is("hello " + uuid));
     }
 
 }
